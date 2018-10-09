ea4918a78b88ff077d3fca7d5f37481ac1ee3d83
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-09 15:29:42 +0200

    exercice5P2JS

---
+++ script.js
@@ -0,0 +1,7 @@
+function rico(url) {
+  document.getElementById('image1').src=url;
+  document.getElementById('image2').src=url;
+  document.getElementById('image3').src=url;
+  document.getElementById('image4').src=url;
+  document.getElementById('image5').src=url;
+}
